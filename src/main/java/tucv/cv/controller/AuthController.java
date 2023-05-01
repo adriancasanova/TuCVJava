2733616290f8e894e25dcdb0406f22380cf6914a
adrian <adriancasanova_@outlook.es>
2023-05-01 20:55:43 -0300

    DEFINITIVO 3

--- src/main/java/tucv/cv/controller/AuthController.java
+++ src/main/java/tucv/cv/controller/AuthController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-  //@CrossOrigin (origins = "http://localhost:4200")
+ // @CrossOrigin (origins = "http://localhost:4200")
   @CrossOrigin (origins = "https://tucv-a4ab0.web.app") 
 public class AuthController {
     @Autowired
